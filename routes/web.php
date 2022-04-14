17ba8ed9e7850adeca1747a10a2905e699a15c76
Dinh Phi Long Nguyen <superitboss@gmail.com>
2022-04-13 20:21:34 -0700

    something

---
+++ routes/web.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Models\Student;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('students', function () {
+    return Student::all();
+});
+
+Route::get('students/{student}', function (Student $student) {
+    return $student;
+});
